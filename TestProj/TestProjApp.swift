a816491f2ecdfb61da6167ba8f20df772043ddaf
A <A>
2023-08-31 12:07:52 +0300

    Initial Commit

---
+++ TestProj/TestProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjApp.swift
+//  TestProj
+//
+//  Created by Вяткин Александр on 31.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
